3072a6e14786fc211435d0ae2ca3a53be53cdf6c
imonweb <contact@imonweb.co.uk>
2022-03-12 23:39:35 +0000

    expenses

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -6,7 +6,7 @@ import "./Expenses.css"
 function Expenses(props) {
   return (
     <div className="expenses">
-      <Expenses>
+      
         <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
 
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
@@ -14,7 +14,7 @@ function Expenses(props) {
         <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
 
         <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
-      </Expenses>
+      
     </div>
   )
 }
