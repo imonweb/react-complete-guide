c4c5b70a30bed3cb2de822cae71e042b8e084c31
imonweb <contact@imonweb.co.uk>
2022-03-13 01:37:48 +0000

    children components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 // import React, { useEffect } from "react"
 import ExpenseDate from "./ExpenseDate"
-import "./ExpenseItems.css"
+import Card from "./Card"
+import "./ExpenseItem.css"
 
 function ExpenseItem(props) {
   // const expenseDate = new Date(2022, 3, 12)
@@ -8,13 +9,13 @@ function ExpenseItem(props) {
   // const expenseAmount = 294.67
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
-    </div>
+    </Card>
   )
 }
 
