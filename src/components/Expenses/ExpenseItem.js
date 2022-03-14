af53df72d3acf74bf91011eb10ada589a78ffdcd
imonweb <contact@imonweb.co.uk>
2022-03-14 18:50:03 +0000

    two way binding

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,33 @@
+// import React, { useEffect } from "react"
+import React, { useState } from "react"
+
+import ExpenseDate from "./ExpenseDate"
+import Card from "../UI/Card"
+import "./ExpenseItem.css"
+
+// function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title)
+  console.log("ExpenseItem evaluated by React")
+
+  // let title = props.title
+
+  const clickHandler = () => {
+    // title = "Updated!"
+    setTitle("Updated!")
+    console.log(title)
+  }
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  )
+}
+
+export default ExpenseItem
---
+++ src/components/UI/Card.js
@@ -0,0 +1,11 @@
+// import React, { useEffect } from "react"
+import "./Card.css"
+
+// function Card(props) {
+const Card = (props) => {
+  const classes = "card " + props.className
+
+  return <div className={classes}>{props.children}</div>
+}
+
+export default Card
