e432e28dd0ab16840e92bba8bd793d8e50a9e6b4
imonweb <contact@imonweb.co.uk>
2022-03-16 00:31:05 +0000

    Conditional content

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -72,6 +72,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submmit">Add Expense</button>
       </div>
     </form>
