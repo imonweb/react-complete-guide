582add523c86584fb1a5833aa0044ebb8f1d3519
imonweb <contact@imonweb.co.uk>
2022-03-16 00:06:43 +0000

    Conditional return statement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 // import React, { useEffect } from "react"
 import React, { useState } from "react"
 // import App from "../App"
-import ExpenseItem from "./ExpenseItem"
+// import ExpenseItem from "./ExpenseItem"
 import Card from "../UI/Card"
 import ExpensesFilter from "./ExpensesFilter"
 import "./Expenses.css"
+import ExpensesList from "./ExpensesList"
 
 // function Expenses(props) {
 const Expenses = (props) => {
@@ -15,13 +16,27 @@ const Expenses = (props) => {
     // props.onChangeFilter(selectedYear)
     setFilteredYear(selectedYear)
   }
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
+  // Method 3
+  // let expensesContent = <p>No expenses found</p>
+
+  // if (filteredExpenses.length > 0) {
+  //   expensesContent = filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)
+  // }
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        ))}
+        <ExpensesList items={filteredExpenses} />
+        {/* {expensesContent} */}
+        {/* //Method 2 {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+        {filteredExpenses.length > 0 && filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)} */}
+        {/* // Method 1 {filteredExpenses.length === 0 ? <p>No expenses found.</p> : filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)} */}
       </Card>
     </div>
   )
